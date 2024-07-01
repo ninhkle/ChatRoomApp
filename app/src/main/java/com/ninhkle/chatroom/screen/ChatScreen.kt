a95c9fc343fb2fc6d3ded1d3a67b242f46c029fa
Ninh Le <ninh.le.94@gmail.com>
2024-07-01 16:10:27 -0700

    add chatscreen

---
+++ app/src/main/java/com/ninhkle/chatroom/screen/ChatScreen.kt
@@ -0,0 +1,147 @@
+package com.ninhkle.chatroom.screen
+
+import android.os.Build
+import android.os.Message
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ninhkle.chatroom.R
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+
+@Composable
+fun ChatScreen(roomId : String) {
+    val text = remember { mutableStateOf("") }
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(16.dp)) {
+        // Display chat messages
+        LazyColumn(modifier = Modifier.weight(1f),content = {
+
+        })
+        // Chat input field
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            BasicTextField(
+                value = text.value,
+                onValueChange = { text.value = it },
+                textStyle = TextStyle.Default.copy(fontSize = 16.sp),
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(8.dp)
+            )
+            IconButton(onClick = {
+                if(text.value.isNotEmpty()) {
+                    text.value = ""
+                }
+            }) {
+                Icon(imageVector = Icons.Default.Send, contentDescription = "Send")
+            }
+        }
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun ChatMessageItem(message: Message) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        horizontalAlignment = /*if (message.isSentByCurrentUser) colorResource(id = R.color.purple_700) else */Color.Gray
+    ) {
+        Box(modifier = Modifier
+            .background(
+                Color.Gray,
+                shape = RoundedCornerShape(8.dp)
+            )
+            .padding(8.dp)) {
+            Text(text = message.text,
+                color = Color.White,
+                style = TextStyle(fontSize = 16.sp)
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ChatScreenPreview() {
+    ChatScreen(roomId = "PreviewID")
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+private fun formatTimestamp(timestamp: Long): String {
+    val messageDateTime = LocalDateTime.ofInstant(Instant.ofEpochMilli(timestamp),
+        ZoneId.systemDefault())
+    val now = LocalDateTime.now()
+    return when {
+        isSameDay(messageDateTime, now) -> "Today ${formatTime(messageDateTime)}"
+        isSameDay(messageDateTime.plusDays(1), now) -> "Yesterday ${formatTime(messageDateTime)}"
+        else -> formatDate(messageDateTime)
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+private fun isSameDay(dateTime1: LocalDateTime, dateTime2: LocalDateTime): Boolean {
+    val formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy")
+    return dateTime1.format(formatter) == dateTime2.format(formatter)
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+private fun formatTime(dateTime: LocalDateTime) : String {
+    val formatter = DateTimeFormatter.ofPattern("HH:mm")
+    return dateTime.format(formatter)
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+private fun formatDate(dateTime: LocalDateTime): String {
+    val formatter = DateTimeFormatter.ofPattern("MMM d, yyyy")
+    return dateTime.format(formatter)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
